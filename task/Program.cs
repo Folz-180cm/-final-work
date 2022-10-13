0077a53be7a39feb13900b61d5458290cd2b1926
Ruslan <folzbuisness@gmail.com>
2022-10-13 12:43:01 +0200

    объявляю метод создания массива

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿//задаю массив строк
+string[] array = {"Russia", "2", "world", ":-)", "-2", "3", "12", "Git"};
+
+//объявляю метод создания массива строк длинной 3+ символа
+string[] CreateArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count += 1;
+        }
+    }
+    string[] array = new string[count];
+    {
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3) 
+            {
+                array[j] = arr[i];
+                j += 1;
+            }
+        }
+    }
+    return array;
+}
